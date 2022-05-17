79a781d8018d719a4f89dd01ec9fbb75f7076903
djalexkidd <alexpyr@outlook.fr>
2022-05-17 10:33:26 +0200

    Météo et favicon

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+const APICALL = "https://api.openweathermap.org/data/2.5/weather?lat=48.5218&lon=-1.32629&units=metric&appid=0"
+const affichage = document.querySelector('.weather-widget')
+
+// Création d'une fonction asynchrone
+async function dataWeather() {
+    const reponse = await fetch(`${APICALL}`)
+    const data = await reponse.json()
+    console.log(data)
+
+    creationWidget(data)
+}
+
+// Affichage du widget météo
+function creationWidget(weather) {
+    const carteHTML = `
+    <p>Température à Saint-James: ${weather.main.temp}°C</p>
+    `
+
+    affichage.innerHTML = carteHTML
+}
+
+/* Envoi de la requête */
+dataWeather()
