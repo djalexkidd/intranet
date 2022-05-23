48e3c286f299a16f9872ff4b019ff70e41db2901
djalexkidd <alexpyr@outlook.fr>
2022-05-23 15:30:35 +0200

    OMG c'est réellement dynamique

--- app.js
+++ app.js
@@ -5,13 +5,26 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.set('view engine', 'ejs');
+
+// Création d'une fonction asynchrone pour la météo
+async function dataWeather() {
+    const APICALL = "https://api.openweathermap.org/data/2.5/weather?lat=48.5218&lon=-1.32629&units=metric&appid=0"
+    const reponse = await fetch(`${APICALL}`)
+    const weather = await reponse.json()
+    return `${weather.main.temp}°C`
+}
 
 app.get("/", (req, res) => {
-    res.render(index.html);
+    dataWeather().then(temp => {
+        res.render('index.ejs', {
+            weather: temp
+        });
+    })
 });
 
 app.get("/form", (req, res) => {
-    res.sendFile(__dirname + '/public/newworker.html');
+    res.render('newworker.ejs');
 });
 
 app.listen(3000, () => console.log("Server is running!"));
