f68bdd0573210a74fe9bdfef6a88f8f7d8272256
djalexkidd <alexpyr@outlook.fr>
2022-05-23 12:26:52 +0200

    C'était l'inverse

--- js/formworker.js
+++ js/formworker.js
@@ -3,7 +3,7 @@ const inputContainer = document.querySelector('.hardware-lend')
 
 for (var i = 0; i < radio.length; ++i) {
     radio[i].onclick = function() {
-      if (this.value == "directe") {
+      if (this.value == "indirecte") {
         inputContainer.style.display = "block"
       } else {
         inputContainer.style.display = "none"
