a6259d26754237b06ec1cd441307ba66bf1c6e97
djalexkidd <alexpyr@outlook.fr>
2022-05-23 10:36:06 +0200

    Pas de prêt quand c'est indirect

---
+++ js/formworker.js
@@ -0,0 +1,12 @@
+const radio = document.getElementsByName('1')
+const inputContainer = document.querySelector('.hardware-lend')
+
+for (var i = 0; i < radio.length; ++i) {
+    radio[i].onclick = function() {
+      if (this.value == "directe") {
+        inputContainer.style.display = "block"
+      } else {
+        inputContainer.style.display = "none"
+      }
+    }
+}
