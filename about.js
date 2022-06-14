71a587517c99804c94b8e0612e3d0ff76709eff5
djalexkidd <alexpyr@outlook.fr>
2022-06-14 12:15:46 +0200

    CSS refactorisé

--- about.js
+++ about.js
@@ -2,9 +2,10 @@ const nodever = process.version
 const pjson = require('./package.json')
 const operatingsystem = process.platform
 
+const arr = [nodever, pjson.version, operatingsystem]
+
 module.exports = {
     getProjectInfo: function (nbr) {
-        const arr = [nodever, pjson.version, operatingsystem]
         return arr[nbr]
     }
 }
