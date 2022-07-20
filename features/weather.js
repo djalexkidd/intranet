3c1391ea26ff5f6ad3b09cffef4480cf40e61566
djalexkidd <alexpyr@outlook.fr>
2022-07-20 18:21:34 +0200

    Météo ne crash plus si indisponible

--- features/weather.js
+++ features/weather.js
@@ -1,9 +1,14 @@
 // Création d'une fonction asynchrone pour la météo
 module.exports = {
     dataWeather: async function () {
-        const APICALL = `https://api.openweathermap.org/data/2.5/weather?lat=48.5218&lon=-1.32629&units=metric&appid=${process.env.WEATHER_APIKEY}`
-        const reponse = await fetch(APICALL)
-        const weather = await reponse.json()
-        return `${weather.main.temp}°C`
+        try {
+            const APICALL = `https://api.openweathermap.org/data/2.5/weather?lat=48.5218&lon=-1.32629&units=metric&appid=${process.env.WEATHER_APIKEY}`
+            const reponse = await fetch(APICALL)
+            const weather = await reponse.json()
+            return `${weather.main.temp}°C`
+        }
+        catch {
+            return "Indisponible"
+        }
     }
 }
